b08bb93e07c0010aadec9914e04a77d4436b023b
7345sh <136170860+7345sh@users.noreply.github.com>
2026-04-01 03:09:09 +0300

    Update admin.py

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Location, Post
+from .models import Category, Location, Post, Comment  # Добавлен Comment
 
 
 admin.site.register(Post)
@@ -16,3 +16,10 @@ class CategoryAdmin(admin.ModelAdmin):
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('name', 'is_published')
     list_editable = ('is_published',)
+
+
+@admin.register(Comment)  # Добавлена регистрация Comment
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('text', 'author', 'post', 'created_at')
+    list_filter = ('created_at', 'author')
+    search_fields = ('text',)
